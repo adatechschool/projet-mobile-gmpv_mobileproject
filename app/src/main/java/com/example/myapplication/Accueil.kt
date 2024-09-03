ea0b3fc7db146674ba8f2e86aaf761df620b2752
geraldinegautier <gvercherand@gmail.com>
2024-09-03 15:31:00 +0200

    add Preview

--- app/src/main/java/com/example/myapplication/Accueil.kt
+++ app/src/main/java/com/example/myapplication/Accueil.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Accueil(navComposable: NavController){
@@ -33,4 +35,11 @@ fun Accueil(navComposable: NavController){
             Text(text = "EXPLORER")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    val navController = rememberNavController()
+    Accueil(navController)
 }
